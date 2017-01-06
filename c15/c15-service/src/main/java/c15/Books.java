ccee30dd8ed609621cd4ee0f9ff6e6eabbb7ad27
madven <selcukc@gmail.com>
2017-01-06 13:59:48 +0300

    c15 testler eksik

---
+++ c15/c15-service/src/main/java/c15/Books.java
@@ -0,0 +1,30 @@
+package c15;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+@XmlRootElement
+@XmlSeeAlso(Book.class)
+public class Books extends ArrayList<Book> {
+
+	public Books() {
+	}
+
+	public Books(Collection<? extends Book> c) {
+		super(c);
+	}
+
+	@XmlElement(name = "book")
+	public List<Book> getBooks() {
+		return this;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.addAll(books);
+	}
+}
